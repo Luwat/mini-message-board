86e5dd0aa4e89e06749e29c7554dcfbd3949bd3c
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-07-30 14:02:12 +0100

    Complete project

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const indexRouter = require('./routes/indexRouter');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
-
+app.use(express.urlencoded({ extended: true}))
 app.use('/', indexRouter);
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
